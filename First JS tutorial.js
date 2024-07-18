7a469de086ece406d94baa52b4ea2377a3338837
Haepaex <kinggold.lego@gmail.com>
2024-07-18 23:12:05 +0100

    Another commit

---
+++ First JS tutorial.js
@@ -0,0 +1,36 @@
+    console.log("Hello Goldking, Welcome your ass to this shit");
+    console.log("In order to succeed, you must nedd absolute consistency");
+    // this is a comment
+    /*This is a 
+    comment
+    comment
+    comment
+    multi-line comment*/
+
+    var a = 27;
+    var b = 22;
+
+    var sum = 10 + 10;
+    console.log(sum);
+
+    var quotes = "and he said \"Do not eat from that plate\" ";
+    console.log(quotes)
+
+    var myString = "firstline\n\tsecondline\n\rthirdline\n\r\bfouthline";
+    console.log(myString);
+
+    var sentenceOne = "I like pizzas ";
+    sentenceOne += "and eba";
+    console.log(sentenceOne);
+
+    sentenceOneLength = sentenceOne.length
+    console.log(sentenceOneLength);
+
+    firstLetterOfSentenceOne = sentenceOne[14];
+    console.log(firstLetterOfSentenceOne);
+
+    lastLetterOfSentenceOne = sentenceOne[sentenceOne.length - 1];
+    console.log(lastLetterOfSentenceOne);
+
+
+    console.log(a * b);
